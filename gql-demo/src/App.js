64128bdd5337beec6556f28f4f9a4f74c3f86516
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-05-09 19:40:45 -0400

    added header styling

--- gql-demo/src/App.js
+++ gql-demo/src/App.js
@@ -7,7 +7,7 @@ import { FIND_STOCK } from "./graphql-operations";
 export default function App(props) {
   const [searchText, setSearchText] = React.useState("MDB");
   const { loading, data } = useQuery(FIND_STOCK, {
-    variables: { query: { "_id":  searchText} },
+    variables: { query: { _id: searchText } },
   });
   const stock = data ? data.basicRecord : null;
   console.log(stock);
@@ -15,29 +15,44 @@ export default function App(props) {
 
   return (
     <div className="App">
-      <h1>Find a Stock</h1>
-      <span className="subheading">
-        The app automatically searches as you type – 
-      </span>
-      <div className="title-input">
-        <input
-          className="fancy-input"
-          value={searchText}
-          onChange={e => setSearchText(e.target.value)}
-          type="text"
-        />
-      </div>
-      {
-        !loading &&
-        !stock && <div className="status">No Stock with that ticker!</div>
-      }
-      {stock && (
-        <div>
-          <h2>{stock.shortName}</h2>
-          <div>Ticker: {stock._id}</div>
-          <br />
+      <div className="header">
+        <div id="main-title-area">
+          <h1 id="page-title">Find a Stock</h1>
         </div>
-      )}
+        <div className="utilities">
+          <button className="utilities-elem" id="upgrade-button">
+            Upgrade
+          </button>
+          <button className="utilities-elem" id="login">
+            Login
+          </button>
+        </div>
+      </div>
+      <div className="search-area">
+        <span className="subheading">
+          The app automatically searches as you type – 
+        </span>
+        <div className="title-input">
+          <input
+            className="fancy-input"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="search-result-area">
+        {!loading && !stock && (
+          <div className="status">No Stock with that ticker!</div>
+        )}
+        {stock && (
+          <div className="current-stock">
+            <h2>{stock.shortName}</h2>
+            <div>Ticker: {stock._id}</div>
+            <br />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
