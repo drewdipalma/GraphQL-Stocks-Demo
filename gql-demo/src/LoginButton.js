a70a7a0f17d4ef1f252e1543513dd8872f67699a
Drew DiPalma <drew.dipalma@mongodb.com>
2020-05-13 23:33:05 -0400

    Updates to State/Upgrade Button

--- gql-demo/src/LoginButton.js
+++ gql-demo/src/LoginButton.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {app} from "./index";
+import {app, client} from "./index";
 import {AnonymousCredential, UserPasswordCredential} from "mongodb-stitch-browser-sdk";
 
 export default class LoginButton extends React.Component {
@@ -10,22 +10,20 @@ export default class LoginButton extends React.Component {
       }
 
     async handleClick(loginInfo) {
-        try{
-            this.state.loggedIn ? 
-            await app.auth.loginWithCredential(new AnonymousCredential()):
-            await app.auth.loginWithCredential(new UserPasswordCredential("foo@bar.com", "Password"));
-        } catch (error){
-            console.log(error);
-        }
-        
-        this.setState(state => ({
-            loggedIn: app.auth.user.loggedInProviderType === 'anon-user' ? false : true
-          }));
+      this.state.loggedIn ? 
+      await app.auth.loginWithCredential(new AnonymousCredential()):
+      await app.auth.loginWithCredential(new UserPasswordCredential("foo@bar.com", "Password"));
+  
+      client.resetStore();
+
+      this.setState(state => ({
+        loggedIn: app.auth.user.loggedInProviderType === 'anon-user' ? false : true
+      }));
     }
   
     render() {
       return (
-        <button onClick={(loginInfo) => this.handleClick(loginInfo)}>
+        <button className="utilities-elem" onClick={(loginInfo) => this.handleClick(loginInfo)}>
           {this.state.loggedIn ? 'Log Out' : 'Log In'}
         </button>
       );
--- gql-demo/src/UpgradeButton.js
+++ gql-demo/src/UpgradeButton.js
@@ -1,13 +1,30 @@
 import * as React from "react";
-import { app } from "./index";
+import { app, client } from "./index";
+import { UPDATE_USER } from "./graphql-operations";
+import { useMutation } from "@apollo/react-hooks";
 
 export default function UpgradeButton() {
-  const [premium, setPremium] = React.useState(
-    app.auth.user.customData.isPremium
-  );
+  const [upgradeUser, { loading: updating }] = useMutation(UPDATE_USER);
+
+  const updateUser = async () => {
+    console.log("foo")
+    await upgradeUser({
+      variables: {
+        query: { _id: app.auth.user.id },
+        set: { premiumUser: !app.auth.user.customData.premiumUser },
+      },
+    });
+
+    client.resetStore();
+
+    await app.auth.refreshAccessToken();
+  };
+
+  console.log(app.auth.user.customData)
+
   return (
-    <button onClick={() => setPremium(!premium)}>
-      {premium ? "Downgrade" : "Upgrade"}
+    <button  className="utilities-elem" onClick={updateUser}>
+      {app.auth.user.customData.premiumUser ? "Downgrade" : "Upgrade"}
     </button>
   );
 }
